1350b008d20b2cc95a910040969fd8722b4d558b
pentonestela <pentonestela@gmai.com>
2024-01-26 10:31:39 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!')
+})
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000')
+})
